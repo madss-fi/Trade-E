96b25ce0e39ef9cacc5dc84fb691bae6d4fd2fa4
madss-fi <madiiculous@gmail.com>
2026-01-10 23:43:09 +0530

    Initial commit: Zerodha project with backend, frontend, dashboard

---
+++ dashboard/src/components/Dashboard.jsx
@@ -0,0 +1,31 @@
+import { Routes, Route } from "react-router-dom";
+
+import Apps from "./Apps";
+import Funds from "./Funds";
+import Holdings from "./Holdings";
+import Orders from "./Orders";
+import Positions from "./Positions";
+import Summary from "./Summary";
+import WatchList from "./WatchList";
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard-container">
+      {/* WatchList rendered directly (no broken context) */}
+      <WatchList />
+
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Summary />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/holdings" element={<Holdings />} />
+          <Route path="/positions" element={<Positions />} />
+          <Route path="/funds" element={<Funds />} />
+          <Route path="/apps" element={<Apps />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ dashboard/src/components/Menu.jsx
@@ -0,0 +1,103 @@
+import React, { use } from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Menu = () => {
+  const [selectedMenu, setSelectedMenu] = useState(0);
+  const [isProfileDropdownOpen, setIsProfileDropdownOpen] = useState(false);
+
+  const handleMenuClick = (index) => {
+    setSelectedMenu(index);
+  };
+
+  const handleProfileClick = (index) => {
+    setIsProfileDropdownOpen(!isProfileDropdownOpen);
+  };
+
+  const menuClass = "menu";
+  const actveMenuClass = "menu selected";
+
+  return (
+    <div className="menu-container">
+      <img src="logo.png" style={{ width: "50px" }} />
+      <div className="menus">
+        <ul>
+          <li>
+            <Link
+              style={{ textDecoration: "none" }}
+              to="/"
+              onClick={() => handleMenuClick(0)}
+            >
+              <p className={selectedMenu === 0 ? actveMenuClass : menuClass}>
+                Dashboard
+              </p>
+            </Link>
+          </li>
+          <li>
+            <Link
+              style={{ textDecoration: "none" }}
+              to="/orders"
+              onClick={() => handleMenuClick(1)}
+            >
+              <p className={selectedMenu === 1 ? actveMenuClass : menuClass}>
+                Orders
+              </p>
+            </Link>{" "}
+          </li>
+          <li>
+            <Link
+              style={{ textDecoration: "none" }}
+              to="/holdings"
+              onClick={() => handleMenuClick(2)}
+            >
+              <p className={selectedMenu === 2 ? actveMenuClass : menuClass}>
+                Holdings
+              </p>
+            </Link>{" "}
+          </li>
+          <li>
+            <Link
+              style={{ textDecoration: "none" }}
+              to="/positions"
+              onClick={() => handleMenuClick(3)}
+            >
+              <p className={selectedMenu === 3 ? actveMenuClass : menuClass}>
+                Positions
+              </p>
+            </Link>{" "}
+          </li>
+          <li>
+            <Link
+              style={{ textDecoration: "none" }}
+              to="/funds"
+              onClick={() => handleMenuClick(4)}
+            >
+              <p className={selectedMenu === 4 ? actveMenuClass : menuClass}>
+                Funds
+              </p>
+            </Link>{" "}
+          </li>
+          <li>
+            <Link
+              style={{ textDecoration: "none" }}
+              to="/apps"
+              onClick={() => handleMenuClick(5)}
+            >
+              <p className={selectedMenu === 5 ? actveMenuClass : menuClass}>
+                Apps
+              </p>
+            </Link>{" "}
+          </li>
+        </ul>
+        <hr />
+        <div className="profile" onClick={handleProfileClick}>
+          <div className="avatar">ZU</div>
+          <p className="username">USERID</p>
+        </div>
+        {isProfileDropdownOpen}
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
