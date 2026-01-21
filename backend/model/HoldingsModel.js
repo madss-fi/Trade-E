8b929c49bc4a185b22babd215413594cf06c7c53
madss-fi <madiiculous@gmail.com>
2026-01-21 21:01:34 +0530

    added DB connection

---
+++ backend/model/HoldingsModel.js
@@ -0,0 +1,4 @@
+const {model} = require("mongoose");
+const {HoldingsSchema} = require('../schema/HoldingsSchema')
+const HoldingsModel = new model("holding",HoldingsSchema);
+module.exports = {HoldingsModel};
