21db95d3f61e3312e3cced719a38d7f44e005ef2
madss-fi <madiiculous@gmail.com>
2026-01-22 22:21:18 +0530

    buil schemas

--- backend/model/HoldingsModel.js
+++ backend/model/HoldingsModel.js
@@ -1,6 +1,6 @@
 const {model} = require("mongoose");
 
-const {HoldingsSchema} = require('../schema/HoldingsSchema');
+const {HoldingsSchema} = require('../schemas/HoldingsSchema');
 
 const HoldingsModel = new model("holding",HoldingsSchema);
 
